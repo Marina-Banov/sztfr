1da39ed064a4ba4a708692b1f581d46c2acbd872
mbanov <m.banov7@gmail.com>
2021-04-07 21:37:58 +0200

    add email link and anonymous auth methods. clean up

--- sztfr-android/app/src/main/java/hr/sztfr/sztfr_android/ui/login/LoginActivity.kt
+++ sztfr-android/app/src/main/java/hr/sztfr/sztfr_android/ui/login/LoginActivity.kt
@@ -1,6 +1,9 @@
 package hr.sztfr.sztfr_android.ui.login
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
+import android.content.SharedPreferences.Editor
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -9,10 +12,15 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.ActionCodeSettings
+import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import hr.sztfr.sztfr_android.BuildConfig.APPLICATION_ID
+import hr.sztfr.sztfr_android.BuildConfig.VERSION_NAME
 import hr.sztfr.sztfr_android.MainActivity
 import hr.sztfr.sztfr_android.R
 import hr.sztfr.sztfr_android.databinding.ActivityLoginBinding
@@ -21,131 +29,98 @@ import hr.sztfr.sztfr_android.databinding.ActivityLoginBinding
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
-    private lateinit var loginViewModel: LoginViewModel
-    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var sharedPreferences: SharedPreferences
     private lateinit var auth: FirebaseAuth
-    private val RC_SIGN_IN = 123
+    private lateinit var googleSignInClient: GoogleSignInClient
+
+    private val onAuthComplete = OnCompleteListener<AuthResult> { task ->
+        if (task.isSuccessful) {
+            navigateToMainActivity()
+        } else {
+            showLoginFailed()
+        }
+    }
+
+    companion object {
+        private const val GOOGLE_SIGN_IN_REQ_CODE = 123
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+        sharedPreferences = this.getPreferences(Context.MODE_PRIVATE)
+        auth = Firebase.auth
+
+        binding.emailSignInButton.setOnClickListener {
+            val email = binding.email.text
+            email?.let {
+                binding.email.clearFocus()
+                firebaseAuthSendEmail(email.toString())
+            }
+        }
 
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken(getString(R.string.default_web_client_id))
             .requestEmail()
             .build()
         googleSignInClient = GoogleSignIn.getClient(this, gso)
-        auth = Firebase.auth
-
-        binding.googleSignInButton.setOnClickListener { signIn() }
-/*
-        val username = findViewById<EditText>(R.id.username)
-        val password = findViewById<EditText>(R.id.password)
-        val login = findViewById<Button>(R.id.login)
-        val loading = findViewById<ProgressBar>(R.id.loading)
-
-        loginViewModel = ViewModelProvider(this, LoginViewModelFactory())
-            .get(LoginViewModel::class.java)
-
-        loginViewModel.loginFormState.observe(this@LoginActivity, Observer {
-            val loginState = it ?: return@Observer
-
-            // disable login button unless both username / password is valid
-            login.isEnabled = loginState.isDataValid
-
-            if (loginState.usernameError != null) {
-                username.error = getString(loginState.usernameError)
-            }
-            if (loginState.passwordError != null) {
-               password.error = getString(loginState.passwordError)
-            }
-        })
-
-        loginViewModel.loginResult.observe(this@LoginActivity, Observer {
-            val loginResult = it ?: return@Observer
-
-            loading.visibility = View.GONE
-            if (loginResult.error != null) {
-                showLoginFailed(loginResult.error)
-            }
-            if (loginResult.success != null) {
-                updateUiWithUser(loginResult.success)
-            }
-            setResult(Activity.RESULT_OK)
-
-            //Complete and destroy login activity once successful
-            finish()
-        })
-
-        username.afterTextChanged {
-            loginViewModel.loginDataChanged(
-                username.text.toString(),
-                password.text.toString()
-            )
+        binding.googleSignInButton.setOnClickListener {
+            startActivityForResult(googleSignInClient.signInIntent, GOOGLE_SIGN_IN_REQ_CODE)
         }
 
-        password.apply {
-            afterTextChanged {
-                loginViewModel.loginDataChanged(
-                    username.text.toString(),
-                    password.text.toString()
-                )
-            }
-
-            setOnEditorActionListener { _, actionId, _ ->
-                when (actionId) {
-                    EditorInfo.IME_ACTION_DONE ->
-                        loginViewModel.login(
-                            username.text.toString(),
-                            password.text.toString()
-                        )
-                }
-                false
-            }
-
-            login.setOnClickListener {
-                loading.visibility = View.VISIBLE
-                loginViewModel.login(username.text.toString(), password.text.toString())
-            }
-        }*/
+        binding.anonymousSignIn.setOnClickListener {
+            auth.signInAnonymously().addOnCompleteListener(this, onAuthComplete)
+        }
     }
 
     override fun onStart() {
         super.onStart()
-        auth.currentUser?.let {
+        if (auth.currentUser != null) {
             navigateToMainActivity()
+        } else {
+            val emailLink = intent.data.toString()
+            val email = sharedPreferences.getString("userEmail", "")
+            email?.let {
+                if (auth.isSignInWithEmailLink(emailLink)) {
+                    auth.signInWithEmailLink(email, emailLink).addOnCompleteListener(this, onAuthComplete)
+                }
+            }
         }
     }
 
-    private fun signIn() {
-        val signInIntent = googleSignInClient.signInIntent
-        startActivityForResult(signInIntent, RC_SIGN_IN)
-    }
+    private fun firebaseAuthSendEmail(email: String) {
+        val actionCodeSettings = ActionCodeSettings.newBuilder()
+                .setHandleCodeInApp(true)
+                .setUrl("https://sztfr.page.link")
+                .setAndroidPackageName(APPLICATION_ID, true, VERSION_NAME)
+                .build()
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if (requestCode == RC_SIGN_IN) {
-            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
-            try {
-                val account = task.getResult(ApiException::class.java)!!
-                firebaseAuthWithGoogle(account.idToken!!)
-            } catch (e: ApiException) {
+        // TODO If you want the dynamic link to redirect to a specific activity, you will need to configure an intent filter in your AndroidManifest.xml file. This can be done by either specifying your dynamic link domain or the email action handler in the intent filter. By default, the email action handler is hosted on a domain like the following example: PROJECT_ID.firebaseapp.com
+        auth.sendSignInLinkToEmail(email, actionCodeSettings).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val editor: Editor = sharedPreferences.edit()
+                editor.putString("userEmail", email)
+                editor.apply()
+                Toast.makeText(applicationContext, R.string.email_sent, Toast.LENGTH_LONG).show()
+            } else {
                 showLoginFailed()
             }
         }
     }
 
-    private fun firebaseAuthWithGoogle(idToken: String) {
-        val credential = GoogleAuthProvider.getCredential(idToken, null)
-        auth.signInWithCredential(credential)
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    navigateToMainActivity()
-                } else {
-                    showLoginFailed()
-                }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == GOOGLE_SIGN_IN_REQ_CODE) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)!!
+                val credential = GoogleAuthProvider.getCredential(account.idToken!!, null)
+                auth.signInWithCredential(credential).addOnCompleteListener(this, onAuthComplete)
+            } catch (e: ApiException) {
+                showLoginFailed()
             }
+        }
     }
 
     private fun navigateToMainActivity() {
@@ -158,18 +133,3 @@ class LoginActivity : AppCompatActivity() {
         Toast.makeText(applicationContext, R.string.login_failed, Toast.LENGTH_SHORT).show()
     }
 }
-/*
-/**
- * Extension function to simplify setting an afterTextChanged action to EditText components.
- */
-fun EditText.afterTextChanged(afterTextChanged: (String) -> Unit) {
-    this.addTextChangedListener(object : TextWatcher {
-        override fun afterTextChanged(editable: Editable?) {
-            afterTextChanged.invoke(editable.toString())
-        }
-
-        override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
-
-        override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
-    })
-}*/
