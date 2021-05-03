e24519fac6208d40436c6cbb1570a2e6ed533530
pavsec <34980273+pavsec@users.noreply.github.com>
2021-05-03 21:19:54 +0200

    display notification when app is opened, open app on notification tap

--- sztfr-android/app/src/main/java/hr/sztfr/sztfr_android/ui/MainActivity.kt
+++ sztfr-android/app/src/main/java/hr/sztfr/sztfr_android/ui/MainActivity.kt
@@ -1,10 +1,19 @@
 package hr.sztfr.sztfr_android.ui
 
+import android.app.*
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.libraries.places.api.Places
+import androidx.core.app.NotificationCompat
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager.POP_BACK_STACK_INCLUSIVE
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation.Model
 import com.google.firebase.auth.FirebaseAuth
 import hr.sztfr.sztfr_android.R
 import hr.sztfr.sztfr_android.data.repository.PlacesRepository
@@ -14,6 +23,12 @@ import hr.sztfr.sztfr_android.ui.login.LoginActivity
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
+    companion object {
+        private const val CHANNEL_ID = "hr.sztfr"
+        private const val NOTIFICATION_ID = 1234
+    }
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +36,9 @@ class MainActivity : AppCompatActivity() {
         PlacesRepository.client = Places.createClient(this)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         setContentView(R.layout.activity_main)
+
+
+        createNotificationChannel()
     }
   
     private fun signOut() {
@@ -28,5 +46,34 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(this, LoginActivity::class.java)
         startActivity(intent)
         finish()
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_name)
+            val descriptionText = getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+
+            val notificationManager: NotificationManager =
+                getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+
+            val intentNottification = Intent(this, MainActivity::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            }
+            val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intentNottification, 0)
+
+            var builder = NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle(getString(R.string.notification_title))
+                .setContentText(getString(R.string.notification_text))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
+            notificationManager.notify(NOTIFICATION_ID, builder.build())
+        }
     }
 }
