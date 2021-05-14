2306a1ec58465e5c98ed92e2049683a5ddfbcf1e
vecimovic <valentina20202@gmail.com>
2021-05-14 14:03:38 +0200

    Update FirestoreRepository.kt

--- sztfr-android/app/src/main/java/hr/sztfr/sztfr_android/ui/FirestoreRepository.kt
+++ sztfr-android/app/src/main/java/hr/sztfr/sztfr_android/ui/FirestoreRepository.kt
@@ -4,6 +4,7 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.DocumentId
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 
@@ -16,16 +17,39 @@ class FirestoreRepository {
     var user = FirebaseAuth.getInstance().currentUser
 
 
-    //get surveys from firebase firestore
-    fun getSurveys() : CollectionReference{
-        var collectionReference = firestoreDB.collection("surveys")
-        return collectionReference
+    //get published surveys from firebase firestore
+    fun getPublishedSurveys() : Query {
+        var query= firestoreDB.collection("surveys").whereEqualTo(
+                "published",
+                true
+        )
+        return query
+    }
+
+    //get unpublished surveys from firebase firestore
+    fun getUnpublishedSurveys() : Query {
+        var query= firestoreDB.collection("surveys").whereEqualTo(
+                "published",
+                false
+        )
+        return query
     }
 
     //get reference for a picture from firebase storage
     fun getImageReference(imageURL: String) : StorageReference{
         var imageReference = storage.getReferenceFromUrl(storageURL + imageURL)
         return imageReference
+    }
+
+    //get reference array for result images
+    fun getResultImageReferences(imageURLs: List<String>) : List<StorageReference>{
+        var imageReferences : MutableList<StorageReference> = mutableListOf()
+        for (img in imageURLs){
+            var ref = storage.getReferenceFromUrl(storageURL + img)
+            imageReferences.add(ref)
+        }
+
+        return imageReferences
     }
 
 
