ef6ef76f631164c1073a629ce8b07c1625123602
vecimovic <valentina20202@gmail.com>
2021-06-03 17:17:41 +0200

    Update SurveyDetailsFragment.kt

--- sztfr-android/app/src/main/java/hr/sztfr/sztfr_android/ui/survey_details/SurveyDetailsFragment.kt
+++ sztfr-android/app/src/main/java/hr/sztfr/sztfr_android/ui/survey_details/SurveyDetailsFragment.kt
@@ -1,6 +1,7 @@
 package hr.sztfr.sztfr_android.ui.survey_details
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,7 +11,10 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.observe
 import androidx.navigation.fragment.findNavController
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 import hr.sztfr.sztfr_android.R
+import hr.sztfr.sztfr_android.data.model.Question
+import hr.sztfr.sztfr_android.data.model.Questions
 import hr.sztfr.sztfr_android.data.repository.UserRepository
 import hr.sztfr.sztfr_android.databinding.FragmentSurveyDetailsBinding
 import hr.sztfr.sztfr_android.util.handleClick
@@ -20,6 +24,7 @@ class SurveyDetailsFragment : Fragment() {
 
     private lateinit var binding: FragmentSurveyDetailsBinding
     private var userRepository = UserRepository.getInstance(FirebaseFirestore.getInstance())
+    private var firestore = FirebaseFirestore.getInstance()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -43,8 +48,24 @@ class SurveyDetailsFragment : Fragment() {
         binding.surveyDetailsGoBackBtn.setOnClickListener { requireActivity().onBackPressed() }
 
         binding.filloutSurveyButton.setOnClickListener {
-            findNavController().navigate(SurveyDetailsFragmentDirections.
-            actionSurveyDetailsFragmentToSurveyWebViewFragment(viewModel.surveyModel.value!!.googleFormsURL))
+            firestore.collection("surveys").document(viewModel.surveyModel.value!!.documentId)
+                    .collection("questions")
+                    .orderBy("order", Query.Direction.ASCENDING)
+                    .get()
+                    .addOnSuccessListener {
+                        var questions : Questions = Questions()
+                        for(document in it){
+                            var q = document.toObject(Question::class.java)
+                            questions.add(q)
+
+                        }
+                        findNavController().navigate(SurveyDetailsFragmentDirections.
+                        actionSurveyDetailsFragmentToSurveyQuestionsFragment(questions,viewModel.surveyModel.value!! ))
+                    }
+                    .addOnFailureListener{
+                        Log.d("SurveyDetailsFragment", it.toString())
+                    }
+
         }
 
         binding.favoritesButton.setOnClickListener {
