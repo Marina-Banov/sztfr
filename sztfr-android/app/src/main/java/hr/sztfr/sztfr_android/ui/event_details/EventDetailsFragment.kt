b95ae2a3227cfd534e7d5e7339a60a465cca3f68
mbanov <m.banov7@gmail.com>
2021-06-06 11:49:41 +0200

    show map

--- sztfr-android/app/src/main/java/hr/sztfr/sztfr_android/ui/event_details/EventDetailsFragment.kt
+++ sztfr-android/app/src/main/java/hr/sztfr/sztfr_android/ui/event_details/EventDetailsFragment.kt
@@ -7,6 +7,10 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.material.chip.Chip
 import com.google.firebase.firestore.FirebaseFirestore
 import hr.sztfr.sztfr_android.R
@@ -18,7 +22,9 @@ import hr.sztfr.sztfr_android.util.handleClick
 class EventDetailsFragment : Fragment() {
 
     private lateinit var binding: FragmentEventDetailsBinding
+    private lateinit var viewModel: EventDetailsViewModel
     private var userRepository = UserRepository.getInstance(FirebaseFirestore.getInstance())
+    private var googleMap: GoogleMap? = null
 
     override fun onCreateView(inflater: LayoutInflater,
                               container: ViewGroup?,
@@ -26,9 +32,10 @@ class EventDetailsFragment : Fragment() {
         val application = requireActivity().application
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_event_details, container, false)
         binding.lifecycleOwner = this
+
         val event = EventDetailsFragmentArgs.fromBundle(requireArguments()).event
         val viewModelFactory = EventDetailsViewModelFactory(event, application)
-        val viewModel = ViewModelProvider(this, viewModelFactory)
+        viewModel = ViewModelProvider(this, viewModelFactory)
                 .get(EventDetailsViewModel::class.java)
         binding.viewModel = viewModel
 
@@ -37,20 +44,66 @@ class EventDetailsFragment : Fragment() {
             viewModel.updateFavorites(it.favorites)
         })
 
-        viewModel.event.observe(viewLifecycleOwner, {
-            for (tag in (it!!).tags) {
-                val chip = layoutInflater.inflate(R.layout.layout_chip, binding.tagGroup, false) as Chip
-                chip.text = tag
-                chip.isClickable = false
-                binding.tagGroup.addView(chip)
-            }
-        })
+        if (!event.online) {
+            initMapView(savedInstanceState)
+        }
+
+        for (tag in event.tags) {
+            val chip = layoutInflater.inflate(R.layout.layout_chip, binding.tagGroup, false) as Chip
+            chip.text = tag
+            chip.isClickable = false
+            binding.tagGroup.addView(chip)
+        }
 
         binding.favoritesButton.setOnClickListener {
             handleClick(viewModel.event.value!!.documentId)
         }
+
         binding.goBackBtn.setOnClickListener { requireActivity().onBackPressed() }
+
         return binding.root
     }
 
+    private fun initMapView(savedInstanceState: Bundle?) {
+        binding.mapView.onCreate(savedInstanceState)
+        binding.mapView.onResume()
+        binding.mapView.getMapAsync {
+            googleMap = it
+            val pos = LatLng(
+                viewModel.event.value?.googlePlace?.latLng!!.latitude,
+                viewModel.event.value?.googlePlace?.latLng!!.longitude
+            )
+            googleMap!!.addMarker(MarkerOptions().position(pos))
+            googleMap!!.moveCamera(CameraUpdateFactory.newLatLngZoom(pos, 15f))
+        }
+        binding.mapView.visibility = View.VISIBLE
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (googleMap != null) {
+            binding.mapView.onResume()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (googleMap != null) {
+            binding.mapView.onPause()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (googleMap != null) {
+            binding.mapView.onDestroy()
+        }
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        if (googleMap != null) {
+            binding.mapView.onLowMemory()
+        }
+    }
 }
