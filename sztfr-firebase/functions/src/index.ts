8eabe84945fb713c752d5de8bb7d11efce9c4ad4
mbanov <m.banov7@gmail.com>
2021-06-10 15:36:26 +0200

    survey function delete subcollections

--- sztfr-firebase/functions/src/index.ts
+++ sztfr-firebase/functions/src/index.ts
@@ -1,6 +1,7 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 import * as user from "./user";
+import * as surveyFns from "./surveys";
 import { crudOperations } from "./crud";
 
 admin.initializeApp();
@@ -10,3 +11,4 @@ export const userDeleted = user.userDeleted;
 export const enums = functions.https.onRequest(crudOperations("enums"));
 export const events = functions.https.onRequest(crudOperations("events"));
 export const surveys = functions.https.onRequest(crudOperations("surveys"));
+export const deleteSurvey = surveyFns.deleteSubcollections;
