58904dfeb7a622eb477cc5fc6200df9e0a01f24f
mbanov <m.banov7@gmail.com>
2021-06-11 22:40:05 +0200

    update subscribers (this is bad and i'm a sad pikachu)

--- sztfr-firebase/functions/src/index.ts
+++ sztfr-firebase/functions/src/index.ts
@@ -1,14 +1,15 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 import * as user from "./user";
-import * as surveyFns from "./surveys";
+import * as firestore from "./firestore";
 import { crudOperations } from "./crud";
 
 admin.initializeApp();
 
 export const newUserSignUp = user.newUserSignUp;
 export const userDeleted = user.userDeleted;
+export const updatedFavorites = user.updatedFavorites;
+export const deleteSubcollections = firestore.deleteSubcollections;
 export const enums = functions.https.onRequest(crudOperations("enums"));
 export const events = functions.https.onRequest(crudOperations("events"));
 export const surveys = functions.https.onRequest(crudOperations("surveys"));
-export const deleteSurvey = surveyFns.deleteSubcollections;
