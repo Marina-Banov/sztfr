0d7a119209a7fd33c42145cdf63bc5d8811f8449
pavsec <34980273+pavsec@users.noreply.github.com>
2021-06-14 18:44:36 +0200

    open mail app on feedback button click

--- sztfr-android/app/src/main/java/hr/sztfr/sztfr_android/ui/info/InfoFragment.kt
+++ sztfr-android/app/src/main/java/hr/sztfr/sztfr_android/ui/info/InfoFragment.kt
@@ -1,10 +1,13 @@
 package hr.sztfr.sztfr_android.ui.info
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import hr.sztfr.sztfr_android.R
 
 class InfoFragment : Fragment() {
@@ -15,5 +18,18 @@ class InfoFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_info, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        var btn_feedback = view.findViewById<Button>(R.id.feedback_btn)
+        btn_feedback.setOnClickListener {
+            var email_info = "mailto:m.banov7@gmail.com?subject=SZTFR -  pitanja"
+            var intent = Intent(Intent.ACTION_SENDTO).apply {
+                setData(Uri.parse(email_info))
+            }
+            startActivity(intent)
+        }
     }
 }
